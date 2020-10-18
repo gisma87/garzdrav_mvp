c4b3a287d7d59a47bcb9979943f027be4850eb31
stivman <gisma87@yandex.ru>
2020-10-18 21:28:39 +0700

    Updates

---
+++ precache-manifest.b9da6fcd153592343d200b72e93db45d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d768bd827d608b80a71e41912fb3c862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d1db00a86ac5f7447b3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af7e68f46a4eec35b940",
+    "url": "/static/js/2.d7900eb3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d7900eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1db00a86ac5f7447b3",
+    "url": "/static/js/main.8411470c.chunk.js"
+  },
+  {
+    "revision": "e12481b91733013490ff",
+    "url": "/static/js/runtime-main.93d47b2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
