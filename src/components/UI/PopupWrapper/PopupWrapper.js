a04223e60b8c7b6a00ba5f5a8fe9fd78b0abdbe6
stivman <gisma87@yandex.ru>
2020-11-13 10:01:18 +0700

    popupLogin

--- src/components/UI/PopupWrapper/PopupWrapper.js
+++ src/components/UI/PopupWrapper/PopupWrapper.js
@@ -5,12 +5,13 @@ import SvgClose from "../icons/SvgClose";
 
 const PopupWrapper = props => {
   return (
-    <div className='PopupWrapper'>
-      {props.active && <Backdrop onClick={props.onClick}/>}
-      <div className="PopupWrapper__content">
+    <div className={'PopupWrapper' + (props.active ? ' PopupWrapper_is-opened' : '')}>
+      <Backdrop onClick={props.onClick}/>
+      <div className={"PopupWrapper__content " + props.classStyle}>
         <div className="PopupWrapper__close" onClick={props.onClick}>
           <SvgClose
             className='closeIcon'
+            onClick={props.onClick}
             // color='#000'
           />
         </div>
