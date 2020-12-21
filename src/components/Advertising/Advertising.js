82988e81a993d907c1005c7f3de6c141f7a847a2
stivman <gisma87@yandex.ru>
2020-12-21 17:31:06 +0700

    Advertising - главный слайдер

--- src/components/Advertising/Advertising.js
+++ src/components/Advertising/Advertising.js
@@ -9,14 +9,6 @@ import test2 from '../../img/slider/test2.jpg'
 import test3 from '../../img/slider/test3.jpg'
 import test4 from '../../img/slider/test4.jpg'
 
-// const ImageBlock = ({image, color}) => {
-//   return (
-//     <div className='Advertising__box' style={{backgroundColor: color}}>
-//       <img className='Advertising' src={image} alt={'slide ' + Math.random()}/>
-//     </div>
-//   )
-// }
-
 const imgData = [
   {src: test1, color: '#f1adba'},
   {src: test2, color: '#0068b9'},
@@ -28,25 +20,31 @@ const Advertising = () => {
   const isMobile = useMediaQuery({query: '(max-width: 800px)'})
   SwiperCore.use([Navigation, Pagination, Autoplay])
   return (
-    <Swiper
-      spaceBetween={0}
-      slidesPerView={1}
-      tag="section" wrapperTag="ul"
-      // navigation
-      loop={true}
-      pagination={!isMobile && {clickable: true}}
-      autoplay={{delay: 5000}}
-      style={{width: '100%'}}
-    >
-      {imgData.map((item, index) => {
-        return (
-          <SwiperSlide tag="li" key={index} style={{display: 'flex', justifyContent: 'center'}}>
-            {/*<ImageBlock image={item.src} color={item.color}/>*/}
-            <img className='Advertising' src={item.src} alt={index + ' slide'}/>
-          </SwiperSlide>
-        )
-      })}
-    </Swiper>
+    <div className='Advertising wrapper'>
+      <div className='Advertising__slider'>
+        <Swiper
+          spaceBetween={0}
+          slidesPerView={1}
+          tag="section" wrapperTag="ul"
+          // navigation
+          loop={true}
+          pagination={!isMobile && {clickable: true}}
+          autoplay={{delay: 5000}}
+          style={{width: '100%'}}
+        >
+          {imgData.map((item, index) => {
+            return (
+              <SwiperSlide tag="li" key={index} style={{display: 'flex', justifyContent: 'center'}}>
+                <img className='Advertising__slide' src={item.src} alt={index + ' slide'}/>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
+
+      <div className='Advertising__promo'> </div>
+    </div>
+
   )
 }
 
