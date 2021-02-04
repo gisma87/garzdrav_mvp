82c44616f70e95a453c2a0db2dd1acef4d431e15
stivman <gisma87@yandex.ru>
2021-02-04 15:48:22 +0700

    cities mobile

--- src/containers/CitiesMobile/CitiesMobile.js
+++ src/containers/CitiesMobile/CitiesMobile.js
@@ -1,16 +1,11 @@
 import React, {useEffect, useState} from "react"
 import {YMaps, Map, Placemark, Clusterer, GeolocationControl, SearchControl} from "react-yandex-maps";
-import iconLoc from '../../img/test/map-pin.svg'
-import iconGZ from '../../img/iconmap/gz.png'
-import iconDA from '../../img/iconmap/da.png'
 import './CitiesMobile.scss'
 import {fetchRetailsCity, setIsCity} from "../../actions";
 import {connect} from "react-redux";
 import BlockWrapper from "../../components/BlockWrapper";
-import Select from "../../components/UI/Select/Select";
-import iconEV from "../../img/iconmap/evalar.png";
-import iconOP from "../../img/iconmap/op.png";
-import iconBS from "../../img/iconmap/bs.png";
+import iconLegko from "../../img/icon/legkoIconSmall.png";
+import SelectDropdown from "../../components/UI/SelectDropdown/SelectDropdown";
 
 const CitiesMobile = props => {
 
@@ -43,23 +38,6 @@ const CitiesMobile = props => {
     }
   }
 
-  function setIcon(type) {
-    switch (type) {
-      case 'Гармония Здоровья':
-        return iconGZ;
-      case 'Дешёвая Аптека':
-        return iconDA;
-      case 'Эвалар':
-        return iconEV;
-      case 'Очень Оптика':
-        return iconOP;
-      case 'ВСЕМ (В7)':
-        return iconBS;
-      default:
-        return iconLoc;
-    }
-  }
-
   useEffect(() => {
     props.fetchRetailsCity()
   }, [isCity.guid])// eslint-disable-line
@@ -78,27 +56,27 @@ const CitiesMobile = props => {
     controls: ['zoomControl', 'fullscreenControl']
   };
 
-  const selectChangeHandler = (event) => {
-    const guid = event.target.value
-    const city = cities.find(element => element.guid === guid)
-    console.log(city);
-    props.setIsCity(city)
-  }
+  const arrayCitiesForDropdown = () => props.cities.map(el => ({id: el.guid, value: el.title}))
 
   return (
     <div className='CitiesMobile wrapper'>
       <header>
         <h1>Аптеки</h1>
-        <p>Ваш город: </p>
-        <Select
-          value={isCity.guid}
-          onChange={selectChangeHandler}
-          options={cities.map(city => {
-            return {text: city.title, value: city.guid}
-          })
+        <div className='CitiesMobile__dropdownCities'>
+          <p>Город:</p>
+          {
+            arrayCitiesForDropdown()?.length > 0
+            && <SelectDropdown items={arrayCitiesForDropdown()}
+                               activeElement={arrayCitiesForDropdown().find(el => el.id === isCity.guid)}
+                               selectItem={(guid) => {
+                                 const itemCity = cities.find(el => el.guid === guid)
+                                 props.setIsCity(itemCity)
+                               }}
+            />
           }
-        />
+        </div>
       </header>
+
 
       <div className='CitiesMobile__mainContainer'>
         <div className='CitiesMobile__menu'>
@@ -129,7 +107,9 @@ const CitiesMobile = props => {
                   }}
                 >
                   <span className='CitiesMobile__itemTitle'>{item.brand}</span>
-                  <span className='CitiesMobile__itemAddress'>{item.street} {item.buildNumber}</span>
+                  <span className='CitiesMobile__itemAddress'>
+                    <span onClick={() => setView(true)}>{item.street} {item.buildNumber}</span>
+                  </span>
                   <span className='CitiesMobile__textClock'>Часы работы:&nbsp;{clock}</span>
                   <span className='CitiesMobile__textClock'>Тел.:&nbsp;{item.phone}</span>
                 </li>
@@ -138,50 +118,56 @@ const CitiesMobile = props => {
           </ul>
         </BlockWrapper>}
 
-        {view && <YMaps>
-          <Map className='CitiesMobile__mapContainer'
-               state={mapState}
-               modules={['control.ZoomControl', 'control.FullscreenControl', "templateLayoutFactory", "layout.ImageWithContent"]}
-          >
-            <Clusterer
-              options={{
-                preset: 'islands#invertedVioletClusterIcons',
-                groupByCoordinates: false,
-              }}
-            >
-              {
-                props.retailsCity.map((item) => {
-                  const {coordinates, guid, brand, city, street, buildNumber, phone} = item
-                  const clockArr = item.weekDayTime.match(/\d\d:\d\d/g) || []
-                  let clock = ''
-                  if (clockArr.length > 1) {
-                    clock = clockArr.join(' - ')
+        {
+          <div style={view ? {visibility: 'visible'} : {visibility: 'hidden'}}>
+            <YMaps>
+              <Map className='CitiesMobile__mapContainer'
+                   state={mapState}
+                   modules={['control.ZoomControl', 'control.FullscreenControl', "templateLayoutFactory", "layout.ImageWithContent"]}
+              >
+                <Clusterer
+                  options={{
+                    preset: 'islands#nightClusterIcons',
+                    groupByCoordinates: false,
+                  }}
+                >
+                  {
+                    props.retailsCity.map((item) => {
+                      const {coordinates, guid, brand, city, street, buildNumber, phone} = item
+                      const clockArr = item.weekDayTime.match(/\d\d:\d\d/g) || []
+                      let clock = ''
+                      if (clockArr.length > 1) {
+                        clock = clockArr.join(' - ')
+                      }
+                      const popup = {
+                        title: brand,
+                        address: `г. ${city},  ${street} ${buildNumber}`,
+                        clock,
+                        tel: phone
+                      }
+                      return <Placemark key={guid}
+                        // onClick={() => onItemClick(guid)}
+                                        {...placeMark(popup)}
+                                        geometry={coordinates}
+                                        options={{
+                                          // iconLayout: 'default#imageWithContent',
+                                          iconLayout: 'default#image',
+                                          iconImageHref: iconLegko, // своя иконка
+                                          iconImageSize: [45, 61], // размеры нашей иконки
+                                          iconImageOffset: [-22, -61], // сдвиг иконки
+                                          hideIconOnBalloonOpen: false, //запрет на скрытие метки по клику на балун
+                                          balloonOffset: [3, -30] // сдвиг балуна
+                                        }}
+                      />
+                    })
                   }
-                  const popup = {
-                    title: brand,
-                    address: `г. ${city},  ${street} ${buildNumber}`,
-                    clock,
-                    tel: phone
-                  }
-                  return <Placemark key={guid}
-                    // onClick={() => onItemClick(guid)}
-                                    {...placeMark(popup)}
-                                    geometry={coordinates}
-                                    options={{
-                                      // iconLayout: 'default#imageWithContent',
-                                      iconLayout: 'default#image',
-                                      iconImageHref: setIcon(brand),
-                                      iconImageSize: [45, 61],
-                                      iconImageOffset: [-22, -61],
-                                    }}
-                  />
-                })
-              }
-            </Clusterer>
-            <GeolocationControl options={{float: 'left'}}/>
-            <SearchControl options={{float: 'right'}}/>
-          </Map>
-        </YMaps>}
+                </Clusterer>
+                <GeolocationControl options={{float: 'left'}}/>
+                <SearchControl options={{float: 'right'}}/>
+              </Map>
+            </YMaps>
+          </div>
+        }
 
       </div>
     </div>
