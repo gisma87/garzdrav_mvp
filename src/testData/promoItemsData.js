443fcbd750b5434e381e3dc31e1cadddaad3eee1
stivman <gisma87@yandex.ru>
2021-02-24 12:18:47 +0700

    add img for promoBlock

--- src/testData/promoItemsData.js
+++ src/testData/promoItemsData.js
@@ -8,6 +8,36 @@ import img7 from './promoIMG/АЛЕРАНА МАСКА ДВОЛОС ИНТЕНС
 import img8 from './promoIMG/АЛЕРАНА СТИМУЛЯТОР РОСТА БРОВЕЙ И РЕСНИЦ 6МЛ.Х2 [ALERANA].jpg'
 import img9 from './promoIMG/АЛЕРАНА СЫВОРОТКА ДРОСТА ВОЛОС 100МЛ. [ALERANA].jpg'
 import img10 from './promoIMG/АЛЕРАНА ШАМПУНЬ PH-БАЛАНС УВЛАЖН. 250МЛ. [ALERANA].jpg'
+import img11 from './promoIMG/АЛЕРАНА ШАМПУНЬ БИО КЕРАТИН ВОССТАН. 250МЛ. [ALERANA].png'
+import img12 from './promoIMG/АЛЕРАНА ШАМПУНЬ ДЖИРН.КОМБИНИР. ВОЛОС 250МЛ. [ALERANA].png'
+import img13 from './promoIMG/АЛЕРАНА ШАМПУНЬ ДОКРАШ.ВОЛОС 250МЛ. [ALERANA].jpg'
+import img14 from './promoIMG/АЛЕРАНА ШАМПУНЬ ДСУХ. И НОРМ. ВОЛОС 250МЛ. [ALERANA].jpg'
+import img15 from './promoIMG/АЛЕРАНА ШАМПУНЬ ИНТЕНС.ПИТАНИЕ 250МЛ. [ALERANA].jpg'
+import img16 from './promoIMG/АЛЕРАНА ШАМПУНЬ МУЖ. АКТИВАТОР РОСТА 250МЛ. [ALERANA].jpg'
+import img17 from './promoIMG/АЛЕРАНА ШАМПУНЬ МУЖ. ЕЖЕДН. УХОД 250МЛ. [ALERANA].jpg'
+import img18 from './promoIMG/АЛЕРАНА ШАМПУНЬ ПЛОТНОСТЬ+ОБЪЕМ 250МЛ. [ALERANA].jpg'
+import img19 from './promoIMG/АЛЕРАНА ШАМПУНЬ ППЕРХОТИ 250МЛ. [ALERANA].png'
+import img20 from './promoIMG/БИФИФОРМ №30 КАПС. КШРАСТВ..jpg'
+import img21 from './promoIMG/ВЕРТУМ ЛОР 0,255МГДОЗА 40МЛ. СПРЕЙ ДМЕСТ.ПРИМ. ДОЗИР..jpg'
+import img22 from './promoIMG/ВИБРОЦИЛ 15МЛ. НАЗАЛ.КАПЛИ ФЛ.КАП..jpg'
+import img23 from './promoIMG/ГЕКСОРАЛ.jpeg'
+import img24 from './promoIMG/ДЖОНСОНС БЭЙБИ КРЕМ ДЕТ. 3В1 С МОЛОКОМ 50Г. [JOHNSON`S].jpg'
+import img25 from './promoIMG/ДЖОНСОНС БЭЙБИ МАСЛО ДЕТ. 200МЛ. АРТ.20798 [JOHNSON`S].png'
+import img26 from './promoIMG/ДЖОНСОНС БЭЙБИ ПРИСЫПКА.jpeg'
+import img27 from './promoIMG/ДЖОНСОНС БЭЙБИ ШАМПУНЬ РОМАШКА 300МЛ. АРТ.21030 [JOHNSON`S].jpg'
+import img28 from './promoIMG/ДИФЛЮКАН 150МГ. №1 КАПС. ПФАЙЗЕР.png'
+import img29 from './promoIMG/ДИФЛЮКАН 150МГ. №4 КАПС. ПФАЙЗЕР.png'
+import img30 from './promoIMG/ИНГАЛЯТОР CN-233 КОМПРЕССОРНЫЙ [AND].jpg'
+import img31 from './promoIMG/ИНГАЛЯТОР ПАРОВОЙ AEROBREZE CS-001.jpg'
+import img32 from './promoIMG/ИРРИГАТОР ДПОЛОСТИ РТА AQUAJET LD-A8.png'
+import img33 from './promoIMG/КАРДИОМАГНИЛ 75МГ.+15,2МГ. №100 ТАБ..webp'
+import img34 from './promoIMG/ЛАЗОЛВАН 15МГ5МЛ. 100МЛ. №1 СИРОП ДЕТС. ЛЕСНАЯ ЯГОДА ФЛ. БЕРИНГЕР.png'
+import img35 from './promoIMG/МАГНЕ В6 ФОРТЕ №40 ТАБ. ППО.webp'
+import img36 from './promoIMG/МЕДИКА СОНИК ПУЛЬСАР ЗУБ.ЩЕТКА CS-167-B ЧЕРН. [OMRON CS MEDICA].jpg'
+import img37 from './promoIMG/МЕДИКА СОНИК ПУЛЬСАР ЗУБ.ЩЕТКА CS-167-W БЕЛ. [OMRON CS MEDICA].jpg'
+import img38 from './promoIMG/НО-ШПА ФОРТЕ 80МГ. №24 ТАБ. ХИНОИН.png'
+import img39 from './promoIMG/ОМРОН ТОНОМЕТР RS1 АВТОМАТ АРТ.HEM-6120-RU[OMRON].jpg'
+import img40 from './promoIMG/ОТИПАКС 15МЛ16Г. КАПЛИ УШНЫЕ ФЛ.КАП..jpg'
 
 const promoItemsData = [
   {
@@ -49,6 +79,126 @@ const promoItemsData = [
   {
     guid: "d50f47d1-a3cb-4126-b7ff-bc56a2859422",
     img: img10
+  },
+  {
+    guid: "d9e1943b-2bb2-4fc4-9ccb-f641e18e91ad",
+    img: img11
+  },
+  {
+    guid: "1227eb0c-76aa-4f00-b652-f3e621345e62",
+    img: img12
+  },
+  {
+    guid: "3a1f8044-7dde-4e9c-8792-4a49af6a0ed8",
+    img: img13
+  },
+  {
+    guid: "73cdc8bf-9dfd-4bba-ae85-4032c8d219ef",
+    img: img14
+  },
+  {
+    guid: "fe76ab27-d667-4a5e-8b0f-dd6860272fa7",
+    img: img15
+  },
+  {
+    guid: "60541c51-3a43-4436-a1eb-8074ffeb69bc",
+    img: img16
+  },
+  {
+    guid: "1c88233b-7a95-402b-a192-379b829c64c8",
+    img: img17
+  },
+  {
+    guid: "224783ad-10dd-4cca-a3cd-309e56ccd0d1",
+    img: img18
+  },
+  {
+    guid: "fab079f5-737c-4fd1-99b2-209e885624a8",
+    img: img19
+  },
+  {
+    guid: "f101aa8a-6f09-4779-95dd-c9d07e3d1832",
+    img: img20
+  },
+  {
+    guid: "a7744072-9d73-4b40-8f3d-82dd65ced6a7",
+    img: img21
+  },
+  {
+    guid: "3a175033-f077-414f-9818-d90805fc595b",
+    img: img22
+  },
+  {
+    guid: "13e32e18-2808-4cd9-a8b2-f9b01c6e39ae",
+    img: img23
+  },
+  {
+    guid: "41371367-2f05-4c65-b572-12620e0725a3",
+    img: img24
+  },
+  {
+    guid: "840a11eb-ba42-4b42-98df-90c5f1c1bea6",
+    img: img25
+  },
+  {
+    guid: "a3a89834-37dc-4dfd-b430-022bdc32177c",
+    img: img26
+  },
+  {
+    guid: "534bbd97-720f-4f10-b442-e6a770b0286d",
+    img: img27
+  },
+  {
+    guid: "e34f1a93-e972-44ea-bf2b-ea4fdf248765",
+    img: img28
+  },
+  {
+    guid: "9c33a0dc-f290-4858-8692-96e1d3c16663",
+    img: img29
+  },
+  {
+    guid: "92cce02e-d33f-4c19-97d9-41d6cf9fcc32",
+    img: img30
+  },
+  {
+    guid: "f63d4a4c-6a1c-4fcb-bac1-0602552cc707",
+    img: img31
+  },
+  {
+    guid: "6f8c156c-1325-4502-bb5e-535b0fc55387",
+    img: img32
+  },
+  {
+    guid: "ce7240de-f9a2-40da-82fb-f32992bcb6bb",
+    img: img33
+  },
+  {
+    guid: "95fbd6c1-514a-4294-9d48-1fc6905e7c61",
+    img: img34
+  },
+  {
+    guid: "38802bbe-0586-434a-9639-207fb9240ebd",
+    img: img35
+  },
+  {
+    guid: "a6669f0d-8b00-47d1-8e91-64868067927b",
+    img: img36
+  },
+  {
+    guid: "3d3ffd3b-a26f-419e-bd86-cddbabd7b11e",
+    img: img37
+  },
+  {
+    guid: "9447b97b-6b28-4d7c-84b1-78dde238c30c",
+    img: img38
+  },
+  {
+    guid: "6b8f7600-dbe0-47a1-866e-6175680096bd",
+    img: img39
+  },
+  {
+    guid: "32204f64-f387-4245-aa0c-1539233d83ca",
+    img: img40
   },
 ]
 
